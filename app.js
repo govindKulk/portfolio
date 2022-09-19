36439c96644e3d46ebc4e199ce8097058f2c3a5e
Govind <kulkarnigovind2003@gmail.com>
2022-09-20 02:47:47 +0530

    styled intro section completely and made navbar for mobile devices(uncomplete)

---
+++ app.js
@@ -0,0 +1,7 @@
+// let btn = document.getElementById('btn');
+// let nav = document.getElementsByTagName('nav')[0];
+
+// btn.addEventListener("click", function(){
+//     nav.style.display = 'block';
+//     console.log('btn');
+// })
