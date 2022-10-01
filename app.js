780dbde954d6bc9a221ce0d51cee612a23be4fd5
Govind <kulkarnigovind2003@gmail.com>
2022-10-02 00:47:32 +0530

    added my work section with animation

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ navToggle.addEventListener('click',() => {
     document.body.classList.toggle('nav-open');
 })
 
+//You can also use link =>{} instead of (link) => {} since parameter is only one
+ 
 navLinks.forEach((link)=>{link.addEventListener('click', ()=>{
     document.body.classList.remove('nav-open');
 })})
